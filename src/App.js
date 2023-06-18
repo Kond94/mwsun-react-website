37b9d05c427b6bfa15811171509e7949af8e9ed9
Kondwerani Kamsesa <kamsesakond@hotmail.com>
2023-06-18 06:40:38 +0200

    Add Maintenance

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "react-under-construction/build/css/index.css";
+
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import AboutPage from "./pages/AboutPage";
@@ -5,17 +7,66 @@ import AccommodationPage from "./pages/AccommodationPage";
 import AllContext from "./context/AllContext";
 import BanquetingPage from "./pages/BanquetingPage";
 import ConferencingPage from "./pages/ConferencingPage";
+import ContactPage from "./pages/ContactPage";
 import Home from "./pages/Home/Home";
 import RestaurantPage from "./pages/Restaurant/RestaurantPage";
 import ScrollTop from "./components/ScrollTop";
-import { ToastContainer } from "react-toastify";
 import TeamPage from "./pages/TeamPage";
-import ContactPage from "./pages/ContactPage";
+import { ToastContainer } from "react-toastify";
+import UnderConstruction from "react-under-construction";
 
 function App() {
   return (
     <>
-      <AllContext>
+      <UnderConstruction
+        background={{
+          image: "/assets/img/slider/homebg-1.jpg",
+          textColor: "#fff",
+          overlay: {
+            color: "#000",
+            opacity: ".5",
+          },
+        }}
+        logo={{
+          src: "/assets/img/logo/mwsun_logo.png",
+          alt: "alt text",
+        }}
+        title={{
+          text: "Malawi Sun Hotel",
+        }}
+        description={{
+          text: "Our website is under maintenance. We'll be here soon, subscribe to be notified",
+          style: {
+            maxWidth: "440px",
+          },
+        }}
+        subscribe={{
+          placeholder: "Enter your email",
+          buttonText: "Subscribe",
+          onSubmit: (value) => {
+            alert(value + "will be emailed when the website is back up");
+          },
+        }}
+        links={[
+          {
+            url: "https://www.facebook.com/",
+            image: "/assets/img/logo/facebook.png",
+          },
+          {
+            url: "https://www.twitter.com/",
+            image: "/assets/img/logo/twitter.png",
+          },
+          {
+            url: "https://www.instagram.com/",
+            image: "/assets/img/logo/instagram.png",
+          },
+          {
+            url: "mailto:info@malawisunhotel.com",
+            image: "/assets/img/logo/mail.png",
+          },
+        ]}
+      />
+      {/* <AllContext>
         <BrowserRouter>
           <ScrollTop />
           <Routes>
@@ -32,7 +83,7 @@ function App() {
           </Routes>
           <ToastContainer />
         </BrowserRouter>
-      </AllContext>
+      </AllContext> */}
     </>
   );
 }
