daca105b1eda504bcc5fe4b73b3b7f00f92889a3
Kondwerani Kamsesa <kamsesakond@outlook.com>
2024-06-27 11:55:35 +0200

    Change Apple Icon

---
+++ sendSMS.js
@@ -0,0 +1,24 @@
+// const AfricasTalking = require("africastalking");
+
+// // TODO: Initialize Africa's Talking
+// const africastalking = AfricasTalking({
+//   apiKey:
+//     "atsk_8856ecbbeb41f3af140691bac2d6a9f7beb416b2be9aead0e6f5459ac56cba1ac8b155af",
+//   username: "mwsun",
+// });
+
+// const sendSMS = async () => {
+//   // TODO: Send message
+//   try {
+//     const result = await africastalking.SMS.send({
+//       to: "+265899505185",
+//       message: "Hey AT Ninja! Wassup...",
+//       from: "ORANUX_LENDE",
+//     });
+//     console.log(result.Message);
+//   } catch (ex) {
+//     console.error("EX: ", ex);
+//   }
+// };
+
+// sendSMS();
